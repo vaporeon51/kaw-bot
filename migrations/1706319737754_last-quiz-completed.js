25ca3c4809cabe2a720c37f937f4e4849c502316
Lemon <soljinbae1228@gmail.com>
2024-01-27 04:10:50 +0000

    Add cooldown to quiz

---
+++ migrations/1706319737754_last-quiz-completed.js
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.addColumn('users', {
+        last_quiz_completed: {
+            type: 'bigint',
+            notNull: false
+    }
+});};
+
+exports.down = pgm => {
+    pgm.dropColumns('users', ['last_quiz_completed']);
+};
