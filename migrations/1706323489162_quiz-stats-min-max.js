cb295e2a7b35b11b3d8c364357b133d23dde0f11
vaporeon51 <157034886+vaporeon51@users.noreply.github.com>
2024-01-27 11:43:00 -0500

    Add max and min streak to quiz_stats table (#8)
    * Add quiz score to leaderboard and change streak logic
    
    * Add max and min streak to quiz_stats table
    
    * revert change
    
    * new migration

---
+++ migrations/1706323489162_quiz-stats-min-max.js
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.dropTable('quiz_stats');
+    pgm.createTable('quiz_stats', {
+        user_id: { type: 'VARCHAR(1000)', primaryKey: true, references: 'users(id)' },
+        week: {
+            type: 'integer',
+            notNull: true,
+            primaryKey: true,
+        },
+        win_streak: { type: 'INTEGER', notNull: true, default: 0 },
+        max_streak: { type: 'INTEGER', notNull: true, default: 0 },
+        min_streak: { type: 'INTEGER', notNull: true, default: 0 },
+        ranking_value: { type: 'INTEGER', notNull: true, default: 0 },
+        completed_questions: { type: 'integer[]', notNull: true, default: '{}' },
+    });
+};
+
+exports.down = pgm => {
+    pgm.dropColumns('quiz_stats', ['max_streak', 'min_streak'])
+};
